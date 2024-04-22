bee8938215c8105c0fcfe29f7f82b75066c96978
cqb13 <cqb13.main@gmail.com>
2024-04-22 10:45:36 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+pub mod cli;
+
+use cli::{Arg, Cli, Command};
+
+fn main() {
+    let cli = Cli::new().with_default_command("help").with_commands(vec![
+        Command::new("version", "Displays the current version of fmap").with_short('v'),
+        Command::new("install", "Installs the files and directories"),
+        Command::new("all", "Gives all stats found on a repository as json")
+            .with_arg(
+                Arg::new()
+                    .with_name("owner")
+                    .with_short('o')
+                    .with_long("owner")
+                    .with_value_name("OWNER")
+                    .with_help("Owner of the repository"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("repository")
+                    .with_short('r')
+                    .with_long("repository")
+                    .with_value_name("REPOSITORY")
+                    .with_help("Name of the repository"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("output")
+                    .with_short('f')
+                    .with_long("output")
+                    .with_value_name("OUTPUT")
+                    .with_help("File path to save the json"),
+            ),
+        Command::new("downloads", "Gives download count of releases as json")
+            .with_arg(
+                Arg::new()
+                    .with_name("owner")
+                    .with_short('o')
+                    .with_long("owner")
+                    .with_value_name("OWNER")
+                    .with_help("Owner of the repository"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("repository")
+                    .with_short('r')
+                    .with_long("repository")
+                    .with_value_name("REPOSITORY")
+                    .with_help("Name of the repository"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("individual")
+                    .with_short('i')
+                    .with_long("individual")
+                    .with_help("Downloads per release"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("link")
+                    .with_short('l')
+                    .with_long("link")
+                    .with_help("Download links for releases (if not individual then for latest)"),
+            )
+            .with_arg(
+                Arg::new()
+                    .with_name("output")
+                    .with_short('f')
+                    .with_long("output")
+                    .with_value_name("OUTPUT")
+                    .with_help("File path to save the json"),
+            ),
+        Command::new(
+            "releases",
+            "Gives names and download links for all releases as json",
+        )
+        .with_arg(
+            Arg::new()
+                .with_name("owner")
+                .with_short('o')
+                .with_long("owner")
+                .with_value_name("OWNER")
+                .with_help("Owner of the repository"),
+        )
+        .with_arg(
+            Arg::new()
+                .with_name("repository")
+                .with_short('r')
+                .with_long("repository")
+                .with_value_name("REPOSITORY")
+                .with_help("Name of the repository"),
+        )
+        .with_arg(
+            Arg::new()
+                .with_name("output")
+                .with_short('f')
+                .with_long("output")
+                .with_value_name("OUTPUT")
+                .with_help("File path to save the json"),
+        ),
+        Command::new("help", "Helps you with the commands").with_short('h'),
+    ]);
+
+    let command = cli.match_commands();
+
+    match command.name {
+        "version" => cli.version(),
+        "all" => {
+            let owner = command.get_value_of("owner").throw_if_none();
+            let repo = command.get_value_of("repository").throw_if_none();
+            let output = command.get_value_of("output").to_option();
+        }
+        "downloads" => {
+            let owner = command.get_value_of("owner").throw_if_none();
+            let repo = command.get_value_of("repository").throw_if_none();
+            let individual = command.has("individual");
+            let link = command.has("link");
+            let output = command.get_value_of("output").to_option();
+        }
+        "releases" => {
+            let owner = command.get_value_of("owner").throw_if_none();
+            let repo = command.get_value_of("repository").throw_if_none();
+            let output = command.get_value_of("output").to_option();
+        }
+        "help" => cli.help(),
+        _ => cli.help(),
+    }
+}
