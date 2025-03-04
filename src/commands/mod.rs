a7afd50bc54adb42a2c23088c65d9f5d28ea198b
cqb13 <cqb13.main@gmail.com>
2025-03-03 20:55:29 -0500

    fixed big oops

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-pub mod repo;
-pub mod releases;
-pub mod user;
-=======
->>>>>>> refs/remotes/origin/main
 pub mod relations;
 pub mod releases;
 pub mod repo;
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@ pub mod commands;
 pub mod styles;
 pub mod utils;
 
-<<<<<<< HEAD
 use crate::cli::{Arg, Cli, CmdOption, Command};
-=======
-use crate::cli::{Arg, Cli, Command};
->>>>>>> refs/remotes/origin/main
 use crate::commands::relations::{relations_command, RelationType};
 use crate::commands::releases::releases_command;
 use crate::commands::repo::repo_command;
@@ -15,7 +11,6 @@ use crate::commands::user::user_command;
 use crate::utils::{install, validate_and_convert_path, OS};
 
 fn main() {
-<<<<<<< HEAD
     let cli = Cli::new()
         .with_command(Command::new("help", "Prints help information").with_option(
             CmdOption::new("command", "COMMAND", "The command you want help with").optional(),
@@ -178,186 +173,6 @@ fn main() {
                     'd',
                 )),
         );
-=======
-    let cli = Cli::new().with_default_command("help").with_commands(vec![
-        Command::new("version", "Displays the current version of github-stats").with_short('v'),
-        Command::new("install", "Installs the files and directories"),
-        Command::new("repo", "Gives all stats found on a repository as json")
-            .with_arg(
-                Arg::new()
-                    .with_name("user")
-                    .with_short('u')
-                    .with_long("user")
-                    .with_value_name("USER")
-                    .with_help("The user who owns the repository"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("repository")
-                    .with_short('r')
-                    .with_long("repository")
-                    .with_value_name("REPOSITORY")
-                    .with_help("Name of the repository"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("output")
-                    .with_short('o')
-                    .with_long("output")
-                    .with_value_name("OUTPUT")
-                    .with_help("File path to save the json"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("display")
-                    .with_short('d')
-                    .with_long("display")
-                    .with_help("Converts the json to an easier format (will remove some data)"),
-            ),
-        Command::new("releases", "Gives information on github releases")
-            .with_arg(
-                Arg::new()
-                    .with_name("user")
-                    .with_short('u')
-                    .with_long("user")
-                    .with_value_name("USER")
-                    .with_help("The user who owns the repository"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("repository")
-                    .with_short('r')
-                    .with_long("repository")
-                    .with_value_name("REPOSITORY")
-                    .with_help("Name of the repository"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("individual")
-                    .with_short('i')
-                    .with_long("individual")
-                    .with_help("Downloads per release"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("link")
-                    .with_short('l')
-                    .with_long("link")
-                    .with_help("Download links for releases (if not individual then for latest)"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("output")
-                    .with_short('o')
-                    .with_long("output")
-                    .with_value_name("OUTPUT")
-                    .with_help("File path to save the json"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("all")
-                    .with_short('a')
-                    .with_long("all")
-                    .with_help("All json from request"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("display")
-                    .with_short('d')
-                    .with_long("display")
-                    .with_help("Converts the json to an easier format (will remove some data)"),
-            ),
-        Command::new("user", "Gives information about a github user")
-            .with_arg(
-                Arg::new()
-                    .with_name("user")
-                    .with_short('u')
-                    .with_long("user")
-                    .with_value_name("USER")
-                    .with_help("The user you want information on"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("output")
-                    .with_short('o')
-                    .with_long("output")
-                    .with_value_name("OUTPUT")
-                    .with_help("File path to save the json"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("display")
-                    .with_short('d')
-                    .with_long("display")
-                    .with_help("Converts the json to an easier format (will remove some data)"),
-            ),
-        Command::new("followers", "Lists the followers of a github user")
-            .with_arg(
-                Arg::new()
-                    .with_name("user")
-                    .with_short('u')
-                    .with_long("user")
-                    .with_value_name("USER")
-                    .with_help("The user you want information on"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("total")
-                    .with_short('t')
-                    .with_long("total")
-                    .with_value_name("TOTAL")
-                    .with_help("Gives the follower count"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("output")
-                    .with_short('o')
-                    .with_long("output")
-                    .with_value_name("OUTPUT")
-                    .with_help("File path to save the json"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("display")
-                    .with_short('d')
-                    .with_long("display")
-                    .with_help("Converts the json to an easier format (will remove some data)"),
-            ),
-        Command::new("following", "Lists users the user is following")
-            .with_arg(
-                Arg::new()
-                    .with_name("user")
-                    .with_short('u')
-                    .with_long("user")
-                    .with_value_name("USER")
-                    .with_help("The user you want information on"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("total")
-                    .with_short('t')
-                    .with_long("total")
-                    .with_value_name("TOTAL")
-                    .with_help("Gives the following count"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("output")
-                    .with_short('o')
-                    .with_long("output")
-                    .with_value_name("OUTPUT")
-                    .with_help("File path to save the json"),
-            )
-            .with_arg(
-                Arg::new()
-                    .with_name("display")
-                    .with_short('d')
-                    .with_long("display")
-                    .with_help("Converts the json to an easier format (will remove some data)"),
-            ),
-        Command::new("help", "Helps you with the commands").with_short('h'),
-    ]);
->>>>>>> refs/remotes/origin/main
 
     let command = cli.match_commands();
 
@@ -373,15 +188,9 @@ fn main() {
             install(&os);
         }
         "repo" => {
-<<<<<<< HEAD
             let user = command.get_option("user").throw_if_none();
             let repo = command.get_option("repository").throw_if_none();
             let output = command.get_arg("output").to_option();
-=======
-            let user = command.get_value_of("user").throw_if_none();
-            let repo = command.get_value_of("repository").throw_if_none();
-            let output = command.get_value_of("output").to_option();
->>>>>>> refs/remotes/origin/main
             let display = command.has("display");
 
             let output = output_to_path(output);
